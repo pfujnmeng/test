24fccd426a8aecbf15bb56a2925d36d26313a517
pfujnmeng <42994025+pfujnmeng@users.noreply.github.com>
2018-09-29 10:59:13 +0800

    Add files via upload
    123

---
+++ AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  test
+//
+//  Created by jnMeng on 2017/6/29.
+//  Copyright © 2017年 jnMeng. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+    NSLog(@"1");
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  test
+//
+//  Created by jnMeng on 2017/6/29.
+//  Copyright © 2017年 jnMeng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
